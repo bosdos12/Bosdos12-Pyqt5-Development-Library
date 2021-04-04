7cd301d9969f4cb72ec20f61057016d75d45e3cb
bosdos12 <66145209+bosdos12@users.noreply.github.com>
2021-04-05 00:22:28 +0200

    Create DEV_LIB_bosdos12pyqt5.py

---
+++ DEV_LIB_bosdos12pyqt5.py
@@ -0,0 +1,13 @@
+def PercentageValue(p, s, a):
+    width = a.primaryScreen().size().width()
+    height = a.primaryScreen().size().height()
+    if (p > 0) and p <= 100:
+        if s == "h":
+            return int((p/100)*height)
+        elif s == "w":
+            return int((p/100)*width)
+
+        else:
+            print(f"Invalid argument, please enter either 'v' or 'h' as the first parameter,\n{str(s)} is invalid;") 
+    else:
+        print(f"Please enter a percentage value between 0 and 100,\n{str(p)} is invalid;")
